86b7d2c5db339d5518d7fd0b4e3e24f2dc0ec12c
Lok Cheong <lokcheong03@gmail.com>
2021-02-24 20:54:21 +0000

    broken our homepage into smaller components, finished stylying homepage and menu components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import HomePage from './hompage.component';
-import './homepage.style.scss';
+import HomePage from './pages/homepage/homepage.component';
+import './pages/homepage/homepage.style.scss';
 
 function App() {
   return (
