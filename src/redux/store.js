cd58b95b656403099acf614278a8a876f8d500fd
Lok Cheong <lokcheong03@gmail.com>
2021-03-04 21:52:38 +0000

    remove redux logger from prod build

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,14 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+//the process.env gives us the environment when app is running
+//when we run in development mode, we use logger to help show the state changes
+//otherwise, if it is in test/production, we do not apply any middlewares
+if (process.env.NODE_ENV === 'development'){
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
